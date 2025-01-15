15e5a3773c9edfaaa7760f187ae4c7ebcb4ddfce
Sukhpal Singh <116950649+Sukhpal77@users.noreply.github.com>
2025-01-15 20:10:54 +0530

    Update authController.js

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -20,6 +20,7 @@ exports.signup = async (req, res) => {
     res.status(201).json({ message: 'User registered successfully' });
   } catch (err) {
     res.status(500).json({ message: 'Server error' });
+    console.log(err);
   }
 };
 
